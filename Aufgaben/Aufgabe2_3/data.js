ca615dbd7a681e4ceea97ac69f40691db424abef
Immanuel-l <immanuel.leinert1@gmail.com>
2021-05-14 10:18:48 +0200

    Aufgabe 2_3 überarbeitet

--- Aufgaben/Aufgabe2_3/data.js
+++ Aufgaben/Aufgabe2_3/data.js
@@ -1,109 +1,27 @@
 "use strict";
 var Aufgabe2_3_2;
 (function (Aufgabe2_3_2) {
-    Aufgabe2_3_2.data = [
-        {
-            name: "SolarSystem",
-            imageUrl: "bilder/solarsystem.svg",
-            yellowSun: {
-                imageUrl: "bilder/yellow_sun.svg",
-                name: "yellowSun",
-                diameter: 1400000,
-                mass: 333000
-            },
-            blueSun: {
-                imageUrl: "bilder/blue_sun.svg",
-                name: "blueSun",
-                diameter: 12756,
-                mass: 1
-            },
-            redSun: {
-                imageUrl: "bilder/red_sun.svg",
-                name: "redSun",
-                diameter: 12756,
-                mass: 1
-            },
-            planetMercury: {
-                imageUrl: "bilder/planet_mercury.svg",
-                name: "planetMercury",
-                orbitPosition: 1,
-                diameter: 4879,
-                distanceToTheSun: 57.9,
-                mass: 0.055,
-                orbitalTime: 88,
-                moons: 0
-            },
-            planetVenus: {
-                imageUrl: "bilder/planet_venus.svg",
-                name: "planetVenus",
-                orbitPosition: 2,
-                diameter: 12104,
-                distanceToTheSun: 108.2,
-                mass: 0.815,
-                orbitalTime: 225,
-                moons: 0
-            },
-            planetEarth: {
-                imageUrl: "bilder/planet_earth.svg",
-                name: "planetEarth",
-                orbitPosition: 3,
-                diameter: 12756,
-                distanceToTheSun: 149.6,
-                mass: 1,
-                orbitalTime: 365,
-                moons: 1
-            },
-            planetMars: {
-                imageUrl: "bilder/planet_mars.svg",
-                name: "planetMars",
-                orbitPosition: 4,
-                diameter: 6794,
-                distanceToTheSun: 227.9,
-                mass: 0.107,
-                orbitalTime: 687,
-                moons: 2
-            },
-            planetJupiter: {
-                imageUrl: "bilder/planet_jupiter.svg",
-                name: "planetJupiter",
-                orbitPosition: 5,
-                diameter: 142984,
-                distanceToTheSun: 779,
-                mass: 317.8,
-                orbitalTime: 4343.5,
-                moons: 63
-            },
-            planetSaturn: {
-                imageUrl: "bilder/planet_saturn.svg",
-                name: "planetSaturn",
-                orbitPosition: 6,
-                diameter: 120536,
-                distanceToTheSun: 1433,
-                mass: 95.2,
-                orbitalTime: 107675,
-                moons: 61
-            },
-            planetUranus: {
-                imageUrl: "bilder/planet_uranus.svg",
-                name: "planetUranus",
-                orbitPosition: 7,
-                diameter: 51118,
-                distanceToTheSun: 2871,
-                mass: 14.5,
-                orbitalTime: 30587,
-                moons: 27
-            },
-            planetNeptune: {
-                imageUrl: "bilder/planet_neptune.svg",
-                name: "planetNeptune",
-                orbitPosition: 8,
-                diameter: 49528,
-                distanceToTheSun: 4495,
-                mass: 17.1,
-                orbitalTime: 59787,
-                moons: 13
-            }
-        }
-    ];
+    Aufgabe2_3_2.parts = {
+        suns: [
+            { image: "./bilder/yellow_sun.svg", name: "Yellow Sun" },
+            { image: "./bilder/blue_sun.svg", name: "Blue Sun" },
+            { image: "./bilder/red_sun.svg", name: "Red Sun" }
+        ],
+        planets: [
+            { image: "./bilder/planet_mercury.svg", name: "Mercury" },
+            { image: "./bilder/planet_venus.svg", name: "Venus" },
+            { image: "./bilder/planet_earth.svg", name: "Earth" },
+            { image: "./bilder/planet_mars.svg", name: "Mars" },
+            { image: "./bilder/planet_jupiter.svg", name: "Jupiter" },
+            { image: "./bilder/planet_saturn.svg", name: "Saturn" },
+            { image: "./bilder/planet_uranus.svg", name: "Uranus" },
+            { image: "./bilder/planet_neptune.svg", name: "Neptune" }
+        ],
+        comets: [
+            { image: "./bilder/comet_1.svg", name: "Comet1" },
+            { image: "./bilder/comet_2.svg", name: "Comet2" },
+            { image: "./bilder/comet_3.svg", name: "Comet3" }
+        ]
+    };
 })(Aufgabe2_3_2 || (Aufgabe2_3_2 = {}));
 //# sourceMappingURL=data.js.map
