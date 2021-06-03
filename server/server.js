95b6a3b7eb136f6582c2a121ed8521b6aa422e53
Immanuel-l <immanuel.leinert1@gmail.com>
2021-06-03 17:11:48 +0200

    server verändert

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.P_3_1Server = void 0;
 const Http = require("http");
-const url = require("url");
+const Url = require("url");
 var P_3_1Server;
 (function (P_3_1Server) {
     console.log("Starting server"); //Auf der Konsole wird Starting server ausgegeben
@@ -20,18 +20,17 @@ var P_3_1Server;
         console.log("I hear voices!"); //Auf der Konsole wird I hear voices ausgegeben
         _response.setHeader("content-type", "text/html; charset=utf-8"); //die Textsprache wird auf UFT-8 gesetzt
         _response.setHeader("Access-Control-Allow-Origin", "*"); //im header wird der Access-Control-Allow-Origin damit jede seite an diese Seite etwas Senden kann
+        if (_request.url) {
+            let url = Url.parse(_request.url, true);
+            for (let key in url.query) {
+                console.log(key + ":" + url.query[key]);
+                _response.write(key + ":" + url.query[key]);
+            }
+            let jsonString = JSON.stringify(url.query);
+            _response.write(jsonString);
+        }
         _response.write(_request.url); //die Request url wird in den response geschrieben
         _response.end(); //die response wird beendet
     }
-    let adresse = "http://immanuelgis.herokuapp.com/default.htm?jahr=2017&monat=february";
-    //Adresse parsen (umwandeln):
-    let q = url.parse(adresse, true);
-    /*Die parse Methode gibt ein Objekt zurück, dass die URL Eigenschaften enthält. So können die fest definierten Eigenschaften einer URL ausgelesen werden:*/
-    console.log(q.host);
-    console.log(q.pathname);
-    console.log(q.search);
-    /*Die query Eigenschaft gibt ein Ojekt zurück, dass alle query-string Parameter als Eigenschaften besitzt. So können beliebig gesendete Attribute ausgelesen werden:*/
-    var qdata = q.query;
-    console.log(qdata.monat);
 })(P_3_1Server = exports.P_3_1Server || (exports.P_3_1Server = {}));
 //# sourceMappingURL=server.js.map
--- server/server.ts
+++ server/server.ts
@@ -1,5 +1,5 @@
 import * as Http from "http";
-import * as url from "url";
+import * as Url from "url";
 
 export namespace P_3_1Server {
     console.log("Starting server"); //Auf der Konsole wird Starting server ausgegeben
@@ -21,20 +21,18 @@ export namespace P_3_1Server {
         console.log("I hear voices!"); //Auf der Konsole wird I hear voices ausgegeben
         _response.setHeader("content-type", "text/html; charset=utf-8"); //die Textsprache wird auf UFT-8 gesetzt
         _response.setHeader("Access-Control-Allow-Origin", "*"); //im header wird der Access-Control-Allow-Origin damit jede seite an diese Seite etwas Senden kann
+
+        if (_request.url) {
+            let url: Url.UrlWithParsedQuery = Url.parse(_request.url, true);
+            for (let key in url.query) {
+                console.log(key + ":" + url.query[key]);
+                _response.write(key + ":" + url.query[key]);
+            }
+            let jsonString: string = JSON.stringify(url.query);
+            _response.write(jsonString);
+        }
+
         _response.write(_request.url); //die Request url wird in den response geschrieben
         _response.end(); //die response wird beendet
     }
-    
-    let adresse: string = "http://immanuelgis.herokuapp.com/default.htm?jahr=2017&monat=february";
-    //Adresse parsen (umwandeln):
-    let q: url.UrlWithParsedQuery = url.parse(adresse, true);
-
-    /*Die parse Methode gibt ein Objekt zurück, dass die URL Eigenschaften enthält. So können die fest definierten Eigenschaften einer URL ausgelesen werden:*/
-    console.log(q.host);
-    console.log(q.pathname);
-    console.log(q.search);
-
-    /*Die query Eigenschaft gibt ein Ojekt zurück, dass alle query-string Parameter als Eigenschaften besitzt. So können beliebig gesendete Attribute ausgelesen werden:*/
-    var qdata = q.query;
-    console.log(qdata.monat);
 }
