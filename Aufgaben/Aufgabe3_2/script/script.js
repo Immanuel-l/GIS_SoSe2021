a953d9f0308502617d5bc34f68c931fae18d4bce
Immanuel-l <immanuel.leinert1@gmail.com>
2021-06-04 17:17:05 +0200

    Aufgabe3_2

--- Aufgaben/Aufgabe3_2/script/script.js
+++ Aufgaben/Aufgabe3_2/script/script.js
@@ -2,15 +2,27 @@
 var Aufgabe3_2;
 (function (Aufgabe3_2) {
     let url = "https://immanuelgis.herokuapp.com/";
-    let button = document.getElementById("button");
-    button.addEventListener("click", dataTransfer);
-    async function dataTransfer() {
+    let responseText = document.getElementById("responseText");
+    let buttonHtml = document.getElementById("buttonHtml");
+    buttonHtml.addEventListener("click", dataTransferHtml);
+    let buttonJson = document.getElementById("buttonJson");
+    buttonJson.addEventListener("click", dataTransferJson);
+    async function dataTransferHtml() {
         let formData = new FormData(document.forms[0]);
         let query = new URLSearchParams(formData);
-        url = url + "?" + query.toString();
+        url += "/html" + "?" + query.toString();
         let response = await fetch(url);
         let text = await response.text();
         console.log(text);
+        responseText.textContent = text;
+    }
+    async function dataTransferJson() {
+        let formData = new FormData(document.forms[0]);
+        let query = new URLSearchParams(formData);
+        url += "/json" + "?" + query.toString();
+        let response = await fetch(url);
+        let jsonText = await response.json();
+        console.log(jsonText);
     }
 })(Aufgabe3_2 || (Aufgabe3_2 = {}));
 //# sourceMappingURL=script.js.map
--- Aufgaben/Aufgabe3_2/script/script.ts
+++ Aufgaben/Aufgabe3_2/script/script.ts
@@ -1,14 +1,30 @@
 namespace Aufgabe3_2 {
     let url: string = "https://immanuelgis.herokuapp.com/";
-    let button: HTMLElement = document.getElementById("button");
-    button.addEventListener("click", dataTransfer);
+    let responseText: HTMLElement = document.getElementById("responseText");
 
-    async function dataTransfer(): Promise<void> {
+    let buttonHtml: HTMLElement = document.getElementById("buttonHtml");
+    buttonHtml.addEventListener("click", dataTransferHtml);
+
+    let buttonJson: HTMLElement = document.getElementById("buttonJson");
+    buttonJson.addEventListener("click", dataTransferJson);
+
+    async function dataTransferHtml(): Promise<void> {
         let formData: FormData = new FormData(document.forms[0]);
         let query: URLSearchParams = new URLSearchParams(<any>formData);
-        url = url + "?" + query.toString();
+        url += "/html" + "?" + query.toString();
         let response: Response = await fetch(url);
         let text: string = await response.text();
         console.log(text);
+        responseText.textContent = text;
+    }
+
+    async function dataTransferJson(): Promise<void> {
+        let formData: FormData = new FormData(document.forms[0]);
+        let query: URLSearchParams = new URLSearchParams(<any>formData);
+        url += "/json" + "?" + query.toString();
+        let response: Response = await fetch(url);
+        let jsonText: string = await response.json();
+        console.log(jsonText);
+        
     }
 }
