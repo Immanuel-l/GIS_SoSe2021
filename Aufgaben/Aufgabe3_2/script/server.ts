5a4668e14520d0e7a1f3c0940c94aa4b6c3bf15d
Immanuel-l <immanuel.leinert1@gmail.com>
2021-06-06 13:38:58 +0200

    test

--- Aufgaben/Aufgabe3_2/script/server.ts
+++ Aufgaben/Aufgabe3_2/script/server.ts
@@ -22,21 +22,20 @@ export namespace Server {
         _response.setHeader("content-type", "text/html; charset=utf-8"); //die Textsprache wird auf UFT-8 gesetzt
         _response.setHeader("Access-Control-Allow-Origin", "*"); //im header wird der Access-Control-Allow-Origin damit jede seite an diese Seite etwas Senden kann
 
-        if (_request.url) {
-            let url: Url.UrlWithParsedQuery = Url.parse(_request.url, true);
-            if (url.pathname == "/html") {
-                for (let key in url.query) {
-                    console.log(key + ":" + url.query[key]);
-                    _response.write(key + ":" + url.query[key]);
-                }
+        let url: Url.UrlWithParsedQuery = Url.parse(_request.url, true);
+        if (url.pathname == "/html") {
+            for (let key in url.query) {
+                console.log(key + ":" + url.query[key]);
+                _response.write(key + ":" + url.query[key]);
             }
-            if (url.pathname == "/json") {
-                let jsonString: string = JSON.stringify(url.query);
-                _response.write(jsonString);
-            }
+            _response.write("hallo");
+        }
+        if (url.pathname == "/json") {
+            let jsonString: string = JSON.stringify(url.query);
+            _response.write(jsonString);
         }
 
-        _response.write("hallo");
+
 
         _response.write(_request.url); //die Request url wird in den response geschrieben
         _response.end(); //die response wird beendet
