d0f5ad6fafc72f50b80e7e826df59db0ac9f5857
Immanuel-l <immanuel.leinert1@gmail.com>
2021-06-20 11:56:47 +0200

    request test

--- Aufgaben/Aufgabe3_4/script/script.js
+++ Aufgaben/Aufgabe3_4/script/script.js
@@ -1,7 +1,7 @@
 "use strict";
 var Aufgabe3_4;
 (function (Aufgabe3_4) {
-    // let responseText: HTMLElement = document.getElementById("responseText");
+    let responseText = document.getElementById("responseText");
     let buttonSend = document.getElementById("buttonSend");
     buttonSend.addEventListener("click", dataSend);
     let buttonRequest = document.getElementById("buttonRequest");
@@ -11,19 +11,15 @@ var Aufgabe3_4;
         let formData = new FormData(document.forms[0]);
         let query = new URLSearchParams(formData);
         url += "?" + query.toString();
-        let response = await fetch(url);
-        let text = await response.text();
-        console.log(text);
-        // responseText.textContent = text;
+        await fetch(url);
     }
     async function dataRequest() {
         let url = "https://immanuelgis.herokuapp.com/request";
-        let formData = new FormData(document.forms[0]);
-        let query = new URLSearchParams(formData);
-        url += "?" + query.toString();
         let response = await fetch(url);
-        let jsonText = await response.json();
-        console.log(jsonText);
+        let text = await response.text();
+        console.log(text);
+        responseText.textContent = text;
+        // console.log(jsonText); 
         // responseText.textContent = jsonText.firstname + " " + jsonText.name + " " + jsonText.matrikelnummer;
     }
     // interface Student {
--- Aufgaben/Aufgabe3_4/script/script.ts
+++ Aufgaben/Aufgabe3_4/script/script.ts
@@ -1,5 +1,5 @@
 namespace Aufgabe3_4 {
-    // let responseText: HTMLElement = document.getElementById("responseText");
+    let responseText: HTMLElement = document.getElementById("responseText");
     let buttonSend: HTMLElement = document.getElementById("buttonSend");
     buttonSend.addEventListener("click", dataSend);
     let buttonRequest: HTMLElement = document.getElementById("buttonRequest");
@@ -10,20 +10,16 @@ namespace Aufgabe3_4 {
         let formData: FormData = new FormData(document.forms[0]);
         let query: URLSearchParams = new URLSearchParams(<any>formData);
         url += "?" + query.toString();
-        let response: Response = await fetch(url);
-        let text: string = await response.text();
-        console.log(text);
-        // responseText.textContent = text;
+        await fetch(url);
     }
 
     async function dataRequest(): Promise<void> {
         let url: string = "https://immanuelgis.herokuapp.com/request";
-        let formData: FormData = new FormData(document.forms[0]);
-        let query: URLSearchParams = new URLSearchParams(<any>formData);
-        url += "?" + query.toString();
         let response: Response = await fetch(url);
-        let jsonText: string = await response.json();
-        console.log(jsonText); 
+        let text: string = await response.text();
+        console.log(text);
+        responseText.textContent = text;
+        // console.log(jsonText); 
         // responseText.textContent = jsonText.firstname + " " + jsonText.name + " " + jsonText.matrikelnummer;
     }
 
