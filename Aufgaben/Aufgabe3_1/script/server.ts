1290bcd10115b1b42b49fbb0eead3637604388a0
Immanuel-l <immanuel.leinert1@gmail.com>
2021-06-20 10:54:08 +0200

    Server der Aufgaben

---
+++ Aufgaben/Aufgabe3_1/script/server.ts
@@ -0,0 +1,25 @@
+import * as Http from "http";
+
+export namespace Server {
+    console.log("Starting server"); //Auf der Konsole wird Starting server ausgegeben
+    let port: number = Number(process.env.PORT); //es wird der port festgelegt
+    if (!port)
+        port = 8100;  //wenn es keinen port gibt wird der port auf 8100 gestellt
+
+    let server: Http.Server = Http.createServer(); //es wird ein http server erstellt
+    server.addListener("request", handleRequest); //dem server wird ein Listener hinzugefügt der die Anfragen vom Benutzer verarbeitet und eine Antwort zurück an den Benutzer schickt
+    server.addListener("listening", handleListen); //dem server wird ein Listener hinzugefügt der abfragt ob der Server grade zuhört
+    server.listen(port); //diese Methode lässt den server auf einen bestimmten port hören
+
+    function handleListen(): void {
+        console.log("Listening"); //in der handleListen funktion wird Listening ausgegeben
+    }
+
+
+    function handleRequest(_request: Http.IncomingMessage, _response: Http.ServerResponse): void {
+        console.log("I hear voices!"); //Auf der Konsole wird I hear voices ausgegeben
+        _response.setHeader("content-type", "text/html; charset=utf-8"); //die Textsprache wird auf UFT-8 gesetzt
+        _response.setHeader("Access-Control-Allow-Origin", "*"); //im header wird der Access-Control-Allow-Origin damit jede seite an diese Seite etwas Senden kann
+        _response.end(); //die response wird beendet
+    }
+}
